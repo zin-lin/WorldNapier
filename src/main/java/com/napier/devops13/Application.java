9ca70bcb12d948a96fee41d3a96038c935d29423
zin-htun <zinlinhtun34@gmail.com>
2023-02-09 17:43:24 +0000

    finalisation

--- src/main/java/com/napier/devops13/Application.java
+++ src/main/java/com/napier/devops13/Application.java
@@ -1,5 +1,8 @@
 package com.napier.devops13;
 
+// Author : Zin Lin Htun
+// Matric : 40542237
+// Application class : main class
 public class Application {
     public static void main (String [] args){
         System.out.println("DEVOPS 13!!!!!!!!!!!");
